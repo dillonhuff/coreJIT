c4f652e16eaa6f1471ab00e95294b2b96b901c67
Dillon Huff <dillonhuff@gmail.com>
2017-11-16 12:23:21 -0800

    Working on creating memory layouts

--- main.cpp
+++ main.cpp
@@ -9,31 +9,10 @@
 
 #include "coreir.h"
 
-
+using namespace CoreIR;
 using namespace std;
 
-int main() {
-  string targetBinary = "./libprog.dylib";
-  ofstream out("./prog.cpp");
-
-  out <<
-    "#include <iostream>\n"
-    "#define EXPORT __attribute__((visibility(\"default\")))\n"
-    "class EXPORT sillyStruct { public:\n\tint i; float j; };\n"
-    "sillyStruct* newSillyStruct() { return new sillyStruct(); }\n"
-    "int sillyFunc(sillyStruct& i) { return i.i; }\n"
-    "int myFunc(int i) {\n"
-    "\treturn 12 + i;\n"
-    "}\n\n"
-    "float otherFunc(float i) {\n"
-    "\treturn i / 12.3;\n"
-    "}\n\n" << endl;
-
-  int ret =
-    system(("clang++ -std=c++11 -fPIC -dynamiclib ./prog.cpp -o " + targetBinary).c_str());
-
-  cout << "clang call ret = " << ret << endl;
-
+int loadLibAndPrint(const std::string& targetBinary) {
   void* myLibHandle = dlopen(targetBinary.c_str(), RTLD_LOCAL);
 
   if (myLibHandle == nullptr) {
@@ -80,4 +59,103 @@ int main() {
 
   dlclose(myLibHandle);
 
+  return 0;
+
+}
+
+vector<vdisc> allInputs(const NGraph& g) {
+  vector<vdisc> inputs;
+  for (auto& vd : g.getVerts()) {
+    if (isGraphInput(g.getNode(vd))) {
+      inputs.push_back(vd);
+    }
+  }
+  return inputs;
+}
+
+vector<vdisc> allOutputs(const NGraph& g) {
+  vector<vdisc> outputs;
+  for (auto& vd : g.getVerts()) {
+    if (isGraphOutput(g.getNode(vd))) {
+      outputs.push_back(vd);
+    }
+  }
+  return outputs;
+}
+
+int main() {
+
+  Context* c = newContext();
+  Namespace* g = c->getGlobal();
+  
+  if (!loadFromFile(c,"./add4.json")) {
+    cout << "Could not Load from json!!" << endl;
+    c->die();
+  }
+
+  c->runPasses({"rungenerators","flattentypes","flatten"});
+
+  Module* m = g->getModule("add4");
+
+  assert(m != nullptr);
+
+  NGraph gr;
+  buildOrderedGraph(m, gr);
+
+  vector<vdisc> ins = allInputs(gr);
+
+  cout << "# of inputs = " << ins.size() << endl;
+
+  assert(ins.size() == 4);
+
+  vector<vdisc> outs = allOutputs(gr);
+
+  cout << "# of outputs = " << ins.size() << endl;
+
+  assert(outs.size() == 1);
+  
+  vector<Type*> bufferLayout;
+  for (auto& vd : ins) {
+    bufferLayout.push_back(gr.getNode(vd).getWire()->getType());
+  }
+  for (auto& vd : outs) {
+    bufferLayout.push_back(gr.getNode(vd).getWire()->getType());
+  }
+
+  vector<int> inputOffsets;
+  int offset = 0;
+  for (auto& tp : bufferLayout) {
+    cout << "Offset = " << offset << endl;
+    inputOffsets.push_back(offset);
+    offset += containerTypeWidth(*tp);
+  }
+
+  string cppCode =
+    "#include <iostream>\n"
+    "#define EXPORT __attribute__((visibility(\"default\")))\n"
+    "class EXPORT sillyStruct { public:\n\tint i; float j; };\n"
+    "sillyStruct* newSillyStruct() { return new sillyStruct(); }\n"
+    "int sillyFunc(sillyStruct& i) { return i.i; }\n"
+    "int myFunc(int i) {\n"
+    "\treturn 12 + i;\n"
+    "}\n\n"
+    "float otherFunc(float i) {\n"
+    "\treturn i / 12.3;\n"
+    "}\n\n";
+
+  string targetBinary = "./libprog.dylib";
+  ofstream out("./prog.cpp");
+  out << cppCode << endl;
+  int ret =
+    system(("clang++ -std=c++11 -fPIC -dynamiclib ./prog.cpp -o " + targetBinary).c_str());
+
+  assert(ret == 0);
+
+  cout << "clang call ret = " << ret << endl;
+
+  int loadRes = loadLibAndPrint(targetBinary);
+
+  deleteContext(c);
+
+  return loadRes;
 }
