8c424ba9f9ddde1f53ff1e6dc9ff97427c04d0f5
Dillon Huff <dillonhuff@gmail.com>
2017-11-21 15:39:37 -0600

    Added more files to gitignore and added mixed jit test

---
+++ test/mixed_jit_tests.cpp
@@ -0,0 +1,19 @@
+#include "catch.hpp"
+
+#include <cassert>
+#include <cstdio>
+#include <iostream>
+#include <string>
+
+#include "coreir/libs/commonlib.h"
+
+#include "corejit.h"
+
+#include "catch.hpp"
+
+using namespace CoreIR;
+using namespace CoreJIT;
+using namespace std;
+
+TEST_CASE("Mixing interpreter and simulator") {
+}
